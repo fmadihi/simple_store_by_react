4d546b12b34f4a141192efc35c6a6cf9939be27c
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2025-05-13 16:48:57 +0330

    Add files via upload

---
+++ src/assets/data.js
@@ -0,0 +1,32 @@
+import labtop01 from './download (1).jfif';
+import labtop02 from './download (2).jfif';
+import labtop03 from './download (3).jfif';
+import labtop04 from './download.jfif';
+
+export const Product = [
+    {
+        id:'1',
+        productName:'labtop:nsdjks',
+        price:'100',
+        productImage:labtop01
+    },
+    {
+        id:'2',
+        productName:'labtop:nsdjks',
+        price:'600',
+        productImage:labtop02
+    },
+    {
+        id:'3',
+        productName:'labtop:nsdjks',
+        price:'1000',
+        productImage:labtop03
+    },
+    {
+        id:'4',
+        productName:'labtop:nsdjks',
+        price:'800',
+        productImage:labtop04
+    },
+
+];
