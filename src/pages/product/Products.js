6a36713de0afbc12bacce30bbc44a24253535ba0
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2025-05-14 16:06:56 +0330

    Update Products.js

--- src/pages/product/Products.js
+++ src/pages/product/Products.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React from "react";
+import { useContext } from "react";
+import { shopContext } from "../../context/shopContext";
 
-const Products = (props) =>{
-    const {id,productName,price,productImage} = props.data
-    return(
-        <div className="col-3">
-            <img src={productImage} />
-                <h5 className="my-1 ">{productName}</h5>
-                <div className="my-1">{price} $</div>
-                <button className="btn btn-info">+</button>
-                <span className="mx-2">0</span>
-                <button className="btn btn-info">-</button>
-        </div>
-    )
-}
+const Products = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  const { cardItems, addItem, removeItem } = useContext(shopContext);
+  const isInCard = cardItems?.some((item) => item.id == id);
+  //const isInCard = !!cardItems?.some((item) => item.id === id); // به boolean تبدیل می‌کنه
+  return (
+    <div className="col-3">
+      <img src={productImage} />
+      <h5 className="my-1 ">{productName}</h5>
+      <div className="my-1">{price} $</div>
+      <button className="btn btn-info" onClick={() => addItem(id)}>
+        +
+      </button>
+      <span className="mx-2">
+        {cardItems?.filter((row) => row.id === id)[0]?.count}
+      </span>
+      {isInCard==true && (
+        <button className="btn btn-info" onClick={() => removeItem(id)}>
+          -
+        </button>
+      )}
+    </div>
+  );
+};
 
-export default Products
+export default Products;
