390c4e3900a2c824cc159506d07716ad4326f36f
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2025-05-14 16:09:15 +0330

    Update Card.jsx

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,6 +1,24 @@
-const Card = () =>{
-    return(
-        <h1>Card</h1>
-    )
-}
-export default Card
+import React from 'react';
+import { useContext } from "react";
+import { Product } from '../assets/data';
+import { shopContext } from '../context/shopContext';
+import Products from './product/Products';
+
+const Card = () => {
+
+    const {cardItems} = useContext(shopContext)
+  return (
+  <React.Fragment>
+      <h1> Your card items... </h1>;
+      <div className="row">
+      {Product.map((el) =>{
+          if(cardItems.some((i)=> i.id === el.id && i.count > 0))
+          return <Products data={el}></Products>
+      })}
+      </div>
+
+
+  </React.Fragment>
+  )
+};
+export default Card;
