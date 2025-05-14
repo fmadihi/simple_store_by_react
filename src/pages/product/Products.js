f7f192cb6565d6ff42ced303c46f6a4d7a46c455
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2025-05-14 09:20:17 +0330

    Update Products.js

--- src/pages/product/Products.js
+++ src/pages/product/Products.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Products = () =>{
-
+const Products = (props) =>{
+    const {id,productName,price,productImage} = props.data
     return(
-        <div>jasbhdxjka</div>
+        <div className="col-3">
+            <img src={productImage} />
+                <h5 className="my-1 ">{productName}</h5>
+                <div className="my-1">{price} $</div>
+                <button className="btn btn-info">+</button>
+                <span className="mx-2">0</span>
+                <button className="btn btn-info">-</button>
+        </div>
     )
 }
 
